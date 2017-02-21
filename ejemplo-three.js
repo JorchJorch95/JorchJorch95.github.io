625ca95da67cc194315d9d9f681814c0814fdcaa
JorchJorch95 <jorgealbertoms@hotmail.com>
2017-02-21 13:58:12 -0600

    Update ejemplo-three.js

--- ejemplo-three.js
+++ ejemplo-three.js
@@ -1,16 +1,8 @@
 var escena = new THREE.Scene();
-
-
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
-
-
 var renderer = new THREE.WebGLRenderer();
-
-
 renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
-
-
 document.body.appendChild( renderer.domElement );
 renderer.render( escena, camara );
 
